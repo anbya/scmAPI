16c4b2c3915ea10add0c58cef0cedd433b037036
anbya <anbyaalibia@gmail.com>
2021-01-29 14:08:48 +0700

    anbya :Fri, Jan 29, 2021  2:08:48 PM

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const app = express()
+const cron = require('node-cron');
+const moment = require('moment-timezone')
 
-const PORT = 5005
+const Centralkitchen = require("./routes/centralkitchen")
+
+const PORT = 3009
 
 app.use(cors());
 app.use(bodyParser.json({limit: '50mb'}));
@@ -13,6 +17,9 @@ app.use(bodyParser.urlencoded({limit: '50mb',extended:false}));
 app.get("/", function(req,res){
     res.send(`Hello World dari armyali.com API ${process.env.TEST} on port:${PORT}`);
 })
+app.use("/centralkitchen", Centralkitchen);
+
+
 app.listen(PORT, () => {
     console.log(`server running on port:${PORT}`);
 });
