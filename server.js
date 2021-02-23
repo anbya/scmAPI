679406eff2153469fcdfcdba6b0d518cd3b5ce4b
anbya <anbyaalibia@gmail.com>
2021-02-23 09:19:18 +0700

    02/23/2021 09:19:18

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-require('dotenv').config()
-const express = require("express")
-const cors = require("cors")
-const bodyParser = require("body-parser")
-const app = express()
+// require('dotenv').config()
+// const express = require("express")
+// const cors = require("cors")
+// const bodyParser = require("body-parser")
+// const app = express()
 
-const Centralkitchen = require("./routes/centralkitchen")
-const Opname = require("./routes/opname")
-const Oauth = require("./routes/oauth")
+// const Centralkitchen = require("./routes/centralkitchen")
+// const Opname = require("./routes/opname")
+// const Oauth = require("./routes/oauth")
 
-const PORT = process.env.PORT || 3009
+// const PORT = process.env.PORT || 3009
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended:false}));
 
-app.get("/", function(req,res){
-    res.send(`Hello World dari JAYGEEGROUP SCM API`);
-})
-app.use("/centralkitchen", Centralkitchen);
-app.use("/opname", Opname);
-app.use("/oauth", Oauth);
+// app.get("/", function(req,res){
+//     res.send(`Hello World dari JAYGEEGROUP SCM API`);
+// })
+// app.use("/centralkitchen", Centralkitchen);
+// app.use("/opname", Opname);
+// app.use("/oauth", Oauth);
 
-app.get("/testinsert", function(req,res){
-    res.send(`Hello World dari SCM API JAYGEEGROUP`);
-})
+// app.get("/testinsert", function(req,res){
+//     res.send(`Hello World dari SCM API JAYGEEGROUP`);
+// })
 
-app.listen(PORT, () => {
-    console.log(`server running on port:${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`server running on port:${PORT}`);
+// });
